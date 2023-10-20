4d188134a69bb4c7e95b037233dacc4847ae089b
elskow <elskow@outlook.com>
2023-10-20 08:50:37 +0700

    chmod: refactor shortener api

--- src/app/api/shorten/route.ts
+++ src/app/api/shorten/route.ts
@@ -1,5 +1,5 @@
-import  {NextResponse} from 'next/server';
-import {nanoid} from 'nanoid';
+import { NextResponse } from 'next/server';
+import { nanoid } from 'nanoid';
 
 import connectMongoDB from '@/utils/connectMongoDb';
 import UrlModels from '@/db/UrlModels';
@@ -8,87 +8,80 @@ const verifyEndpoint =
     'https://challenges.cloudflare.com/turnstile/v0/siteverify';
 const secret = process.env.SECRET_KEY;
 
+async function verifyToken(token: string): Promise<boolean> {
+    if (secret === undefined) {
+        throw new Error('Invalid secret key');
+    }
+
+    const res = await fetch(verifyEndpoint, {
+        method: 'POST',
+        body: `secret=${encodeURIComponent(
+            secret,
+        )}&response=${encodeURIComponent(token)}`,
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+        },
+    });
+
+    const data = await res.json();
+
+    return data.success;
+}
+
+async function createShortUrl(requestData: any): Promise<any> {
+    if (!requestData.url || !requestData.token) {
+        throw new Error('Invalid parameters');
+    }
+
+    const token = requestData.token;
+    const isTokenValid = await verifyToken(token);
+
+    if (!isTokenValid) {
+        throw new Error('Cannot confirm your security code');
+    }
+
+    const ipaddressRes = await fetch('https://api.ipify.org?format=json');
+    const { ip: ipaddress } = await ipaddressRes.json();
+
+    await connectMongoDB();
+
+    let unique;
+    const { url, customAddress } = requestData;
+
+    if (!customAddress) {
+        unique = nanoid(5);
+    } else {
+        const existingUrl = await UrlModels.findOne({ unique: customAddress });
+
+        if (existingUrl) {
+            throw new Error('Custom unique already exists');
+        }
+
+        unique = customAddress;
+    }
+
+    const query = await UrlModels.create({
+        url,
+        unique,
+        ipaddress,
+    });
+
+    return query;
+}
+
 export async function POST(request: Request) {
     try {
         const requestData = await request.json();
+        const data = await createShortUrl(requestData);
 
-        if (!requestData.url && !requestData.token) {
-            return NextResponse.json(
-                {message: 'Invalid parameters'},
-                {status: 400},
-            );
-        }
-
-        const token = requestData.token;
-        let res;
-
-        if (secret !== undefined) {
-            res = await fetch(verifyEndpoint, {
-                method: 'POST',
-                body: `secret=${encodeURIComponent(
-                    secret,
-                )}&response=${encodeURIComponent(token)}`,
-                headers: {
-                    'content-type': 'application/x-www-form-urlencoded',
-                },
-            });
-        } else {
-            return NextResponse.json(
-                {message: 'Invalid secret key'},
-                {status: 400},
-            );
-        }
-
-        const data = await res.json();
-
-        if (data.success) {
-            let url;
-            let unique;
-            const ipaddress = await fetch('https://api.ipify.org?format=json')
-                .then((res) => res.json())
-                .then((res) => res.ip);
-
-            url = requestData.url;
-
-            await connectMongoDB();
-
-            if (!requestData.customAddress) {
-                unique = nanoid(5);
-            } else {
-                const existingUrl = await UrlModels.findOne({
-                    unique: requestData.customAddress,
-                });
-
-                if (existingUrl) {
-                    return NextResponse.json(
-                        {message: 'Custom unique already exists'},
-                        {status: 400},
-                    );
-                }
-
-                unique = requestData.customAddress;
-            }
-
-            const query = await UrlModels.create({
-                url: url,
-                unique: unique,
-                ipaddress: ipaddress,
-            });
-
-            return NextResponse.json(
-                {message: 'Success shorten URL', data: query},
-                {status: 200},
-            );
-        } else {
-            return NextResponse.json(
-                {message: 'Cant confirmation your security code'},
-                {status: 400},
-            );
-        }
-    } catch (error) {
         return NextResponse.json(
-            {message: 'Internal server error'},
-            {status: 500},
+            { message: 'Success shorten URL', data },
+            { status: 200 },
         );
+    } catch (error) {
+        const message = error.message || 'Internal server error';
+        const status = error.status || 500;
+
+        return NextResponse.json({ message }, { status });
     }
 }
